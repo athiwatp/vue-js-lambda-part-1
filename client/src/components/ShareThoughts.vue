8d4e199e241c328cbf7ed48f2f89472182990af6
Bruno Krebs <bruno.krebs@auth0.com>
2018-04-20 08:49:52 -0300

    adding a secured route to Vue.js

---
+++ client/src/components/ShareThoughts.vue
@@ -0,0 +1,29 @@
+<script>
+import MicroPostsService from '../MicroPostsService'
+
+export default {
+  name: 'ShareThoughts',
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    async shareThouths () {
+      await MicroPostsService.insertMicroPost(this.text)
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<template>
+<div class="share-thoughts">
+  <label for="share-thoughts">What do you want to share?</label>
+  <input id="share-thoughts" v-model="text" placeholder="Be nice!"/>
+  <button v-on:click="shareThouths">Share!</button>
+</div>
+</template>
+
+<style scoped>
+</style>
